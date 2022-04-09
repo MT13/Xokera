958ac7adda3d7b82776684c5b51325aa7aa8b8f1
MT13 <maria.tarielashvili@gmail.com>
2022-04-09 16:52:20 +0400

    title page

--- i18n.js
+++ i18n.js
@@ -4,14 +4,15 @@ import LanguageDetector from "i18next-browser-languagedetector";
 const resources = {
   en: {
     translation: {
-      "about game": "About Game",
+      "about game": "About the Game",
       annotation:
-        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat",
-      instruction: "Instruction",
-      "instruction text": "nananananana",
-      "idea and concept": "Concept authors",
-      illustrator: "Illustrator:",
-      developer: "Developer:",
+        "The online game “Cyber Snake” was developed by the Media Development Foundation’s team, while the idea to create an adapted version of the popular game belongs to the contestants of the “Most Media-literate School” competition from the 12th Public School of Gori. (Veriko Michitashvili, Mariam Gozalishvili, Keti Shugliashvili, Giorgi Michitashvili, Luka Shengelia).",
+      instruction: "Instructions",
+      "instruction text": "The game consists of three parts, each of them containing 10 questions. In order to advance to the digital age (III), first, you have to pass through the Gutenberg Era – the realm of print media (I) and the Era of the Broadcasting Media (II). Each correct answer will earn you 10 points and increase the length of the snake. You only have a total of 5 lives. Each incorrect answer will cost you one life. In order to end up in the Digital Era, you would need at least one life and as many points as possible.",
+     questions: "Questions",
+      q_authors: "Media Development Foundation (MDF)",
+      illustrator: "Illustrator: ",
+      developer: "Developer: ",
       "il name": "Zizi Nishnianidze",
       "dev name": "Maria Tarielashvili",
 
@@ -24,11 +25,12 @@ const resources = {
     translation: {
       "about game": "თამაშის შესახებ",
       annotation:
-        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat",
+        "Cyber Snake-ის თამაში მედიის განვითარების ფონდის გუნდმა მოამზადა. პოპულარული თამაშის ადაპტირების იდეა კი “ყველაზე მედიაწიგნიერი სკოლის” კონკურსის მონაწილე გუნდს ეკუთვნის ქ.გორის მე-12 საჯარო სკოლიდან (ვერიკო მიჩიტაშვილი, მარიამ გოზალიშვილი, ქეთი შუღლიაშვილი, გიორგი მიჩიტაშვილი, ლუკა შენგელია).",
       instruction: "თამაშის წესები",
       "instruction text":
         "მედიაწიგნიერი სნეიკი 3 ნაწილისგან შედგება. I საფეხურზე მოთამაშე ბეჭდური მედიის ეპოქაში, II საფეხურზე - სამაუწყებლო, ხოლო III-ზე ციფრული მედიის  ეპოქაში ხვდება. თითოეულ ნაწილში მოთამაშემ 10 შეკითხვას უნდა უპასუხოს.  სწორი პასუხი 10 ქულას უდრის, გველის სიგრძე კი იზრდება. არასწორი პასუხისას 5 სიცოცხლიდან ერთი იკარგება.  მოთამაშე უნდა ეცადოს, მაქსიმალურად მეტი ქულა დააგროვოს, თამაშის ბოლოს შედეგის სახით ნახავს, თუ რამდენი ქულა მოიპოვა.",
-      "idea and concept": "იდეისა და კონცეფციის ავტორები",
+      questions: "კითხვები",
+      q_authors: "მედიის განვითარების ფონდი",
       illustrator: "ილუსტრატორი:",
       developer: "დეველოპერი:",
       "il name": "ზიზი ნიშნიანიძე",
--- src/scenes/TitleScreen.js
+++ src/scenes/TitleScreen.js
@@ -1,23 +1,36 @@
 import Phaser from "phaser";
 import BaseScene from "./BaseScene";
 import i18n from "../../i18n";
+import bg from "../../assets/bg_title.png";
 
 class TitleScreen extends BaseScene {
   constructor() {
     super({ key: "titleScreen" });
   }
+
   preload() {
     this.objects = {};
-    this.load.image("bg", "../../assets/bg_instructions.png");
+    this.load.image("bg", bg);
   }
 
   create() {
-    this.game.state.start('Preloader');
     console.log("in create titleScreen1");
 
     console.log("In titleScreen");
 
-    this.add.image(400, 300, "bg");
+    var windowWidth = window.innerWidth;
+    var widnowHeight = window.innerHeight;
+    this.bg = this.add.image(windowWidth / 2, widnowHeight / 2, "bg");
+    this.bg.setDisplaySize(windowWidth, widnowHeight);
+    var style = {
+      fontFamily: "Roboto_MtavruliBold",
+      fill: "#fff",
+      boundsAlignH: "center",
+      boundsAlignV: "middle",
+    };
+
+    this.myText = this.add.text(50, 50, i18n.t("about game"), style);
+    this.myText.setPadding(5, 5, 5, 5);
   }
 }
 
