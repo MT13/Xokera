00abfc96624fa654c4656ee918ff8dd79a84a1c0
MT13 <maria.tarielashvili@gmail.com>
2022-04-27 15:13:19 +0400

    bla

--- src/constants/title.js
+++ src/constants/title.js
@@ -1,2 +1,2 @@
-export const TITLE_AREA_WIDTH = 1200;
+export const TITLE_AREA_WIDTH = 1500;
 export const TITLE_AREA_HEIGHT = 900;
--- src/main.js
+++ src/main.js
@@ -1,18 +1,17 @@
 import Phaser from "phaser";
 import GameScene from "./scenes/GameScene";
-import { TitleScreen, BackgroundScene} from "./scenes/TitleScreen"
+import { TitleScreen, BackgroundScene } from "./scenes/TitleScreen";
 import i18n from "../i18n";
 import RoundRectanglePlugin from "phaser3-rex-plugins/plugins/roundrectangle-plugin.js";
 import InstructionsScene from "./scenes/InstructionsScene";
 import UIScene from "./scenes/UIScene";
 
-
-const MAX_SIZE_WIDTH_SCREEN = 1920
-const MAX_SIZE_HEIGHT_SCREEN = 1080
-const MIN_SIZE_WIDTH_SCREEN = 480
-const MIN_SIZE_HEIGHT_SCREEN = 360
-const SIZE_WIDTH_SCREEN = 800
-const SIZE_HEIGHT_SCREEN = 600
+const MAX_SIZE_WIDTH_SCREEN = 1920;
+const MAX_SIZE_HEIGHT_SCREEN = 1080;
+const MIN_SIZE_WIDTH_SCREEN = 480;
+const MIN_SIZE_HEIGHT_SCREEN = 360;
+const SIZE_WIDTH_SCREEN = 800;
+const SIZE_HEIGHT_SCREEN = 600;
 
 const config = {
   scale: {
@@ -22,8 +21,8 @@ const config = {
     width: SIZE_WIDTH_SCREEN,
     height: SIZE_HEIGHT_SCREEN,
     min: {
-        width: MIN_SIZE_WIDTH_SCREEN,
-        height: MIN_SIZE_HEIGHT_SCREEN
+      width: MIN_SIZE_WIDTH_SCREEN,
+      height: MIN_SIZE_HEIGHT_SCREEN,
     },
     // max: {
     //     width: MAX_SIZE_WIDTH_SCREEN,
@@ -46,13 +45,16 @@ let game = new Phaser.Game(config);
 
 let titleScreen = new TitleScreen();
 let gameScene = new GameScene();
-let backgroundScene = new BackgroundScene()
-game.scene.add("backgroundScene", backgroundScene)
+let backgroundScene = new BackgroundScene();
+let instructionsScene = new InstructionsScene();
+let uiScene = new UIScene();
+
+game.scene.add("backgroundScene", backgroundScene);
 
 game.scene.add("titleScreen", titleScreen);
 game.scene.add("instructionsScene", instructionsScene);
 // game.scene.add("gameScene", gameScene);
 game.scene.add("uiScene", uiScene);
 
-game.scene.start("backgroundScene")
+game.scene.start("backgroundScene");
 game.scene.start("titleScreen");
